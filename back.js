4175c086f8bd70178632eed0f545d5c36abb78dd
shh79 <shh79@gmail.com>
2021-07-26 17:12:53 +0430

    debug all of problem with one hand :)

--- back.js
+++ back.js
@@ -1,24 +1,26 @@
 function SubmitDo(){
-    //var list=document.getElementsByClassName("DoList");
-    var item=document.createElement("div");
-    item.className="DoItem";
+    var str=document.getElementById("DoTitle").value;
+    if(str=="" || str==undefined){
+        alert("Please fill the blank !!!");
+        return ;
+    }
 
-    var str=document.getElementById("DoTitle").value; 
+    var listOfDo=document.getElementsByClassName("DoList")[0];
+    var item=document.createElement("div");
+    item.className="DoItem"; 
 
     var p=document.createElement("p");
     var temp=document.createTextNode(str);
     p.appendChild(temp);
 
     var button=document.createElement("button");
-    button.value="Done";
-    button.onclick="RemoveItem(button);";
-    //button.onclick="this.parentNode.remove();";
+    button.textContent="Done";
+    button.setAttribute("onclick","this.parentNode.remove();");
 
     item.appendChild(p);
-    item.appendChild(button);
+    item.appendChild(button); console.log(item);
 
-    document.getElementsByClassName("DoList").prepend(item);
-}
-function RemoveItem(element){
-    element.parentNode.Remove();
+    listOfDo.prepend(item);
+
+    document.getElementById("DoTitle").value="";
 }
