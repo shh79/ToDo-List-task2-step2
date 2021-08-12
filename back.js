d264805dd377dc4e4bf60b7030f5cba7fa60ec70
shh79 <shh79@gmail.com>
2021-08-12 20:18:14 +0430

    adding filtering in todo list

--- back.js
+++ back.js
@@ -1,3 +1,34 @@
+let filterType='All';
+function UpdateList(){
+    let items=document.querySelectorAll(".DoItem");
+    switch(filterType){
+        case "All":
+            items.forEach(item => {
+                item.style.display="flex";
+            });
+            break;
+        case "Active":
+            items.forEach(item => {
+                if(item.classList.contains("Completed")){
+                    item.style.display="none";
+                }
+                else{
+                    item.style.display="flex";
+                }
+            });
+            break;
+        case "Completed":
+            items.forEach(item => {
+                if(item.classList.contains("Active")){
+                    item.style.display="none";
+                }
+                else{
+                    item.style.display="flex";
+                }
+            });
+            break;
+    }
+}
 function SubmitDo(){
     var str=document.getElementById("DoTitle").value;
     if(str=="" || str==undefined){
@@ -7,7 +38,8 @@ function SubmitDo(){
 
     var listOfDo=document.getElementsByClassName("DoList")[0];
     var item=document.createElement("div");
-    item.className="DoItem"; 
+    item.className="DoItem";
+    item.classList.add("Active");
 
     var p=document.createElement("p");
     var temp=document.createTextNode(str);
@@ -23,4 +55,19 @@ function SubmitDo(){
     listOfDo.prepend(item);
 
     document.getElementById("DoTitle").value="";
+
+    UpdateList();
+}
+function Filter(button){
+    filterType=button.textContent;
+
+    let filters = document.querySelector(".filters");
+    filters=filters.querySelectorAll("button");
+    filters.forEach(filter => {
+        filter.classList.remove("active");
+    });
+
+    button.classList.add("active");
+
+    UpdateList();
 }
